563ba6deaab2754f913c01a75985234b3591989f
Рафаэль Гантус <rafael.gantus@bk.ru>
2023-12-25 21:10:24 +0300

    fix6

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -42,8 +42,9 @@ module.exports.getCardsId = async (req, res) => {
 
 module.exports.deleteCard = async (req, res) => {
   try {
+    const { cardId } = req.params;
     console.log("deleteCard");
-    const cardId = await Card.findById(req.params.cardId).orFail(() => new NotFoundError(`${ERROR_404}`));
+    await Card.deleteOne({ _id: cardId }).orFail(() => new NotFoundError(`${ERROR_404}`));
     return res.status(http2.constants.HTTP_STATUS_OK).send(cardId);
   } catch (error) {
     if (error.name === "CastError") {
@@ -55,7 +56,7 @@ module.exports.deleteCard = async (req, res) => {
         .send({ message: ERROR_404 });
     }
     return res.status(http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
-      .send({ message: ERROR_500 });
+      .send({ message: ERROR_500, error: error.message });
   }
 };
 
