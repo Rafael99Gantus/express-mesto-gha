152618036664d6fa2fac05a889bce6595cc2a718
Рафаэль Гантус <rafael.gantus@bk.ru>
2023-12-25 20:43:47 +0300

    fix5.1

--- controllers/otherControllers.js
+++ controllers/otherControllers.js
@@ -12,20 +12,25 @@ const ERROR_404 = "Не найдено";
 
 const ERROR_400 = "Переданы некорректные данные";
 
-module.exports.changeLike = (req, res) => {
-  Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-    { new: true },
-  )
-    .then((card) => res.status(http2.constants.HTTP_STATUS_OK).send({ data: card }))
-    .catch((err) => {
-      if (err.name === "CastError") {
-        res.status(http2.constants.HTTP_STATUS_NOT_FOUND).send({ message: ERROR_404 });
-        return;
-      }
-      res.status(http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: ERROR_500 });
-    });
+module.exports.changeLike = async (req, res) => {
+  try {
+    const cardId = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+      { new: true },
+    ).orFail(new NotFoundError({ message: ERROR_404 }));
+    res.status(http2.constants.HTTP_STATUS_OK).send(cardId);
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      res.status(http2.constants.HTTP_STATUS_BAD_REQUEST).send({ message: ERROR_400 });
+      return;
+    }
+    if (error.name === "NotFoundError") {
+      res.status(http2.constants.HTTP_STATUS_NOT_FOUND).send({ message: ERROR_404 });
+      return;
+    }
+    res.status(http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: ERROR_500 });
+  }
 };
 
 module.exports.patchMe = async (req, res) => {
