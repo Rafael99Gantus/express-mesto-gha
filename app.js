43fc452ffe8cab643fc3f924eefc1d38ab7ca968
Рафаэль Гантус <rafael.gantus@bk.ru>
2024-01-20 22:46:49 +0300

    fix

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const errorHandler = require("./middlewares/error");
 const { signUpValidation, signInValidation } = require("./middlewares/celebrate");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
+const allowedCors = [
+  "https://praktikum.tk",
+  "http://praktikum.tk",
+  "localhost:3000",
+];
+
 const ERROR_404 = "Страница не найдена, некорректный запрос";
 
 const app = express();
@@ -23,6 +29,38 @@ mongoose.connect("mongodb://127.0.0.1:27017/mestodb")
   });
 
 app.use(requestLogger);
+
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    // res.header("Access-Control-Allow-Origin", origin);
+    // устанавливаем заголовок, который разрешает браузеру запросы из любого источника
+    res.header("Access-Control-Allow-Origin", "*");
+  }
+
+  next();
+});
+
+// eslint-disable-next-line consistent-return
+app.use((req, res, next) => {
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  // const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  // Если это предварительный запрос, добавляем нужные заголовки
+  const requestHeaders = req.headers["access-control-request-headers"];
+  if (method === "OPTIONS") {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    // res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header("Access-Control-Allow-Headers", requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+  next();
+});
 
 app.post("/signin", signInValidation, login);
 app.post("/signup", signUpValidation, postUser);
