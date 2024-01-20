27cfe898ed755818e131ad6293fcc6d45ab071d0
Рафаэль Гантус <rafael.gantus@bk.ru>
2024-01-20 13:59:27 +0300

    push

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { routerUsers, routerCards } = require("./routes/index");
 const { postUser, login } = require("./controllers/userController");
 const errorHandler = require("./middlewares/error");
 const { signUpValidation, signInValidation } = require("./middlewares/celebrate");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const ERROR_404 = "Страница не найдена, некорректный запрос";
 const { PORT = 3000 } = process.env;
@@ -22,6 +23,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/mestodb")
     console.error("Ошибка подключения:", err.message);
   });
 
+app.use(requestLogger);
+
 app.post("/signin", signInValidation, login);
 app.post("/signup", signUpValidation, postUser);
 
@@ -30,7 +33,11 @@ app.use("/cards", routerCards);
 app.use("*", (req, res, next) => {
   next(new NotFoundError(`${ERROR_404}`));
 });
-app.use(errors());
+
+app.use(errorLogger); // логгер ошибок
+
+app.use(errors()); // обработчик ошибок celebrate
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
